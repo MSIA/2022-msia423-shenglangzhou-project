3b97f919dc1a14fe49a2a9f1d78e2031903ebdb8
dylanz990915 <zz990915@northwestern.edu>
2022-06-04 15:47:57 -0500

    Update flaskconfig.py

--- config/flaskconfig.py
+++ config/flaskconfig.py
@@ -1,13 +1,40 @@
 import os
+from xmlrpc.client import Boolean
+
 DEBUG = True
 LOGGING_CONFIG = "config/logging/local.conf"
-PORT = 5000
-APP_NAME = "penny-lane"
-SQLALCHEMY_TRACK_MODIFICATIONS = True
+PORT = 5004
+APP_NAME = "credit_card_application"
+SQLALCHEMY_Application_MODIFICATIONS = True
 HOST = "0.0.0.0"
 SQLALCHEMY_ECHO = False  # If true, SQL for queries made will be printed
 MAX_ROWS_SHOW = 100
 
+# Connection string
+DB_HOST = os.environ.get('MYSQL_HOST')
+DB_PORT = os.environ.get('MYSQL_PORT')
+DB_USER = os.environ.get('MYSQL_USER')
+DB_PW = os.environ.get('MYSQL_PASSWORD')
+DATABASE = os.environ.get('DATABASE_NAME')
+DB_DIALECT = 'mysql+pymysql'
 SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-if SQLALCHEMY_DATABASE_URI is None:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/tracks.db'
+if SQLALCHEMY_DATABASE_URI is not None:
+    pass
+elif DB_HOST is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///data/application.db'
+else:
+    SQLALCHEMY_DATABASE_URI = '{dialect}://{user}:{pw}@{host}:{port}/{db}'.format(dialect=DB_DIALECT, user=DB_USER,
+                                                                                  pw=DB_PW, host=DB_HOST, port=DB_PORT,
+                                                                                  db=DATABASE)
+
+# Categorical questions used in the app
+
+GENDERS = ['Male', 'Female']
+BINARY = ['Y', 'N']
+BOOL = ['Yes','No']
+INCOME_TYPE = ['Working', 'State servant', 'Commercial associate', 'Pensioner',
+                'Student']
+EDU_TYPE = ['Secondary education', 'Incomplete higher', 'Higher education']
+FAM_STATUS = ['Single / not married', 'Married', 'Civil marriage', 'Widow', 'Separated']
+HOUSING_TYPE = ['Rented apartment', 'House / apartment', 'With parents', 'Municipal apartment', 'Office apartment']
+OCCUPATION_TYPE = ['Sales staff', 'Core staff', 'Laborers','Managers', 'Drivers', 'Not Specified', 'Cooking staff', 'HR staff','High skill tech staff','Low-skill Laborers', 'Medicine staff']
